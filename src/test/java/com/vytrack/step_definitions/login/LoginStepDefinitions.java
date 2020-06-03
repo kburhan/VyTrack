f69bf2aad325f0e72bb6a278c09f408d05d9d7e2
kburhan <kburhan07@gmail.com>
2020-06-03 12:49:54 -0400

    commit

--- src/test/java/com/vytrack/step_definitions/login/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/login/LoginStepDefinitions.java
@@ -1,10 +1,12 @@
 package com.vytrack.step_definitions.login;
 
 import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtilities;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 public class LoginStepDefinitions {
@@ -17,27 +19,27 @@ public class LoginStepDefinitions {
        Driver.getDriver().get(URL);
     }
 
-//    @When("user logs in as a sales manager")
-//    public void user_logs_in_as_a_sales_manager() {
-//        loginPage.login(ConfigurationReader.getProperty("sales_manager"), ConfigurationReader.getProperty("password"),false);
-//
-//    }
-//
-//    @Then("user should verify title is a dashboard")
-//    public void user_should_verify_title_is_a_dashboard() {
-//        BrowserUtilities.waitForPageToLoad(10);
-//        Assert.assertEquals(Driver.getDriver().getTitle(),"Dashboard");
-//    }
-//
-//    @When("user logs in as a store manager")
-//    public void user_logs_in_as_a_store_manager() {
-//        loginPage.login(ConfigurationReader.getProperty("store_manager"), ConfigurationReader.getProperty("password"),false);
-//    }
-//
-//    @When("user logged in as a driver")
-//    public void user_logged_in_as_a_driver() {
-//        loginPage.login(ConfigurationReader.getProperty("driver"), ConfigurationReader.getProperty("password"),false);
-//    }
+    @When("user logs in as a sales manager")
+    public void user_logs_in_as_a_sales_manager() {
+        loginPage.login(ConfigurationReader.getProperty("sales_manager"), ConfigurationReader.getProperty("password"),false);
+
+    }
+
+    @Then("user should verify title is a dashboard")
+    public void user_should_verify_title_is_a_dashboard() {
+        BrowserUtilities.waitForPageToLoad(10);
+        Assert.assertEquals(Driver.getDriver().getTitle(),"Dashboard");
+    }
+
+    @When("user logs in as a store manager")
+    public void user_logs_in_as_a_store_manager() {
+        loginPage.login(ConfigurationReader.getProperty("store_manager"), ConfigurationReader.getProperty("password"),false);
+    }
+
+    @When("user logged in as a driver")
+    public void user_logged_in_as_a_driver() {
+        loginPage.login(ConfigurationReader.getProperty("driver"), ConfigurationReader.getProperty("password"),false);
+    }
 
     @Given("user logs in as a {string}")
     public void user_logs_in_as_a(String username) {
